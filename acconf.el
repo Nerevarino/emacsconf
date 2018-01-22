db74cc8d530b787811ec873e1c164de4e03c5c5d
Evgeniy Dronov <dronov-ep@yandex.ru>
2018-01-22 16:56:26 +0300

    acconf

--- acconf.el
+++ acconf.el
@@ -3,5 +3,5 @@
 (ac-config-default)
 (setq ac-auto-start nil)
 (require 'ac-helm) ;; Not necessary if using ELPA package
-(global-set-key (kbd "C-c C-c") 'ac-complete-with-helm)
-(define-key ac-complete-mode-map (kbd "C-c C-c") 'ac-complete-with-helm)
+(global-set-key (kbd "C-:") 'ac-complete-with-helm)
+(define-key ac-complete-mode-map (kbd "C-:") 'ac-complete-with-helm)
