5165868dc1e5e55639807f25bb3b5a574b79d4d5
Evgeniy Dronov <dronov-ep@yandex.ru>
2018-01-22 17:14:16 +0300

    2018-01-22

--- baseconf.el
+++ baseconf.el
@@ -1,3 +1,9 @@
 (require 'package)
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+;; (load-file "~/.emacs.d/baseconf.elc")
+;; (load-file "~/.emacs.d/helmconf.elc")
+;; (load-file "~/.emacs.d/hgtconf.elc")
+;; (load-file "~/.emacs.d/hyasm.elc")
+;; (load-file "~/.emacs.d/acconf.elc")
